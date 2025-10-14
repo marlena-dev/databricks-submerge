1d6d104dce01eac7b1d30ece01e5c46ba3da054b
marlena <marlenalawrynowicz59@gmail.com>
2025-10-14 16:06:13 -0300

    update

--- dlt_bronze_bitcoin_pipeline-(1)/bronze/sales_commodities.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REFRESH STREAMING LIVE TABLE bronze_yfinance
-TBLPROPERTIES ("quality" = "bronze")
-AS
-SELECT *
-FROM cloud_files(
-  '/Volumes/lakehouse/raw_public/raw_yfinance/commodities/latest_prices',
-  'json'
-);
--- dlt_bronze_bitcoin_pipeline-(1)/bronze/yfinance_bronze.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REFRESH STREAMING LIVE TABLE bronze_yfinance
-TBLPROPERTIES ("quality" = "bronze")
-AS
-SELECT *
-FROM cloud_files(
-  '/Volumes/lakehouse/raw/raw_yfinance/commodities/latest_prices',
-  'json'
-);
